3a6d52c6dd3e36050643f54a4b0a235ff1860b63
leandroricke100 <leandro_henrique_100@hotmail.com>
2023-11-16 18:07:29 -0300

    criando pagina home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,44 @@
 import "./styles.scss";
+import { BsCartPlus } from "react-icons/bs";
 
 export function Home() {
   return (
     <div>
-      <h1>pagina home</h1>
+      <main className="container-main">
+        <h1 className="paragraf-main">Produtos em alta</h1>
+
+        <div className="container-itens">
+          <section className="itens">
+            <img
+              src="https://sujeitoprogramador.com/wp-content/uploads/2023/06/racao1.png"
+              alt="foto da ração"
+            />
+            <p className="title-item">Ração Premier Fórmula para Cães Sênior</p>
+
+            <div className="container-price">
+              <strong className="price">R$5000</strong>
+              <button className="button-cart">
+                <BsCartPlus />
+              </button>
+            </div>
+          </section>
+
+          <section className="itens">
+            <img
+              src="https://sujeitoprogramador.com/wp-content/uploads/2023/06/racao1.png"
+              alt="foto da ração"
+            />
+            <p className="title-item">Ração Premier Fórmula para Cães Sênior</p>
+
+            <div className="container-price">
+              <strong className="price">R$5000</strong>
+              <button className="button-cart">
+                <BsCartPlus />
+              </button>
+            </div>
+          </section>
+        </div>
+      </main>
     </div>
   );
 }
